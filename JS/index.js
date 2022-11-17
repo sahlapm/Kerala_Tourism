8163f51e5b950467ac1cf7157100e556a6e4c7da
sahlapm <117145374+sahlapm@users.noreply.github.com>
2022-11-17 07:48:47 +0300

    Assignment2

--- JS/index.js
+++ JS/index.js
@@ -20,31 +20,57 @@ function validate()
         errContact.innerHTML='Please enter a valid Contact No';
         errContact.style.color="red";
         return false;
+        
     }
-    if(!emailRegex.test(email.value))
+    else
     {
-        errEmail.innerHTML='Please enter a valid email';
-        errEmail.style.color="red";
-        return false;
+        errContact.innerHTML='';
+        if(!emailRegex.test(email.value))
+        {
+            errEmail.innerHTML='Please enter a valid email';
+            errEmail.style.color="red";
+            return false;
+        }
+        else
+        {
+            errEmail.innerHTML='';
+            if(!pwdRegex.test(password.value))
+            {
+                errPassword.innerHTML='Please enter a valid password';
+                errPassword.style.color="red";
+                return false;
+            }
+            else
+            {
+                errPassword.innerHTML="";
+                if(!pwdRegex.test(retypePswd.value))
+                {
+                    errRetype.innerHTML='Please enter a valid password';
+                    errRetype.style.color="red";
+                    return false;
+                }
+                else
+                {
+                    errRetype.innerHTML='';
+                    if(password.value!= retypePswd.value)
+                    {
+                        errPassword.innerHTML='Password and Retype password do not match';
+                        errPassword.style.color="red";
+                        return false;
+                    }
+                    else
+                    {
+                        errPassword.innerHTML='';
+                        return true;
+                    }
+                }
+            }
+        }
     }
+   
 
-    if(!pwdRegex.test(password.value))
-    {
-        errPassword.innerHTML='Please enter a valid password';
-        errPassword.style.color="red";
-        return false;
-    }
-    if(!pwdRegex.test(retypePswd.value))
-    {
-        errRetype.innerHTML='Please enter a valid password';
-        errRetype.style.color="red";
-        return false;
-    }
+   
+   
 
-    if(password.value!= retypePswd.value)
-    {
-        errPassword.innerHTML='Password and Retype password do not match';
-        errPassword.style.color="red";
-        return false;
-    }
+    
 }
